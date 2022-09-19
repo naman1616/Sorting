2d113fb5f5156610a4a1bd7fca1e11a93bb70c3d
naman <naman@LAPTOP-31I7S424>
2022-09-19 10:13:38 +0530

    New Sort

---
+++ src/NewSort.java
@@ -0,0 +1,33 @@
+public class NewSort {
+		// TODO Auto-generated method stub 
+		    static void newSort(int[] arr) {  
+		        int n = arr.length;  
+		        int temp = 0;  
+		         for(int i=0; i < n; i++){  
+		                 for(int j=1; j < (n-i); j++){  
+		                          if(arr[j-1] > arr[j]){  
+		                                 //swap elements  
+		                                 temp = arr[j-1];  
+		                                 arr[j-1] = arr[j];  
+		                                 arr[j] = temp;  
+		                         }      
+		                 }  
+		         }  
+		    }  
+		    public static void main(String[] args) {  
+		                int arr[] ={3,60,35,2,45,320,5};  
+		                 
+		                System.out.println("Array Before New Sort");  
+		                for(int i=0; i < arr.length; i++){  
+		                        System.out.print(arr[i] + " ");  
+		                }  
+		                System.out.println();  
+		                  
+		                newSort(arr); 
+		                 
+		                System.out.println("Array After New Sort");  
+		                for(int i=0; i < arr.length; i++){  
+		                        System.out.print(arr[i] + " ");  
+		                }  	   
+		        }  
+}
